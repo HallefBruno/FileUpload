1b72edc60ee237f5e8a78a4f2caded6ad83949f2
hallef.wantek <hallef.wantek@E00009584.tiradentes.local>
2018-12-11 17:01:04 -0200

    Finalizando outra forma de upload de arquivo csv

--- src/java/newupload/UploadServlet.java
+++ src/java/newupload/UploadServlet.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -24,23 +26,51 @@ public class UploadServlet extends HttpServlet {
         filePart.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(fileContent));
         PrintWriter out = response.getWriter();
-        int contador = 1;
+        int contador = 0;
         StringBuilder sb = new StringBuilder();
+        
+        sb.append("<table>");
+        sb.append("<tr>");
+        sb.append("<th>Nome");sb.append("</th>");
+        sb.append("<th>Idade");sb.append("</th>");
+        sb.append("</tr>");
+        
+        List<Pessoa> addPessoa = new LinkedList<>();
+        Pessoa oEPessoa = new Pessoa();
         while(br.ready()) {
             String colunas[] = br.readLine().split(";");
-            sb.append("<p>");
-            sb.append(colunas[0]);
-            sb.append("</p>");
+            if(contador>0) {
+                oEPessoa.nome = colunas[0];
+                oEPessoa.idade = Integer.valueOf(colunas[1]);
+                addPessoa.add(oEPessoa);
+                oEPessoa = new Pessoa();
+            }
+            contador++;
         }
-        
+        addPessoa.forEach((p) -> {
+            sb.append("<tr>");
+            sb.append("<td>");
+            sb.append(p.getNome());
+            sb.append("</td>");
+            sb.append("</tr>");
+            
+        });
+        addPessoa.forEach((p) -> {
+            sb.append("<tr>");
+            sb.append("<td>");
+            sb.append(p.getIdade());
+            sb.append("</td>");
+            sb.append("</tr>");
+        });
+        sb.append("</table>");
         response.setContentType("text/html");
         response.setHeader("Cache-Control", "no-cache");
         
-        out.write("<html>");
-        out.write("<body>");
-        out.write(sb.toString());
-        out.write("</body>");
-        out.write("</html>");
+        out.println("<html>");
+        out.println("<body>");
+        out.println(sb.toString());
+        out.println("</body>");
+        out.println("</html>");
         
     }
     
@@ -54,5 +84,36 @@ public class UploadServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
         System.out.println("Get");
+    }
+    
+    public class Pessoa {
+        
+        private String nome;
+        private int idade;
+        
+        public Pessoa(String nome, int idade) {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public Pessoa() {
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+        
     }
 }
